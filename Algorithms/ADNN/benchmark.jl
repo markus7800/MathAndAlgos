8b299ff874e95f26679d312083af78c3ffd5156a
Markus Böck <markus.boeck7800@hotmail.com>
2020-11-08 22:23:31 +0100

    conv backward

--- Algorithms/ADNN/benchmark.jl
+++ Algorithms/ADNN/benchmark.jl
@@ -26,7 +26,7 @@ function convolve_slice(W::AbstractArray, b::AbstractArray, stride::Tuple{Int,In
 	return output
 end
 
-function convolve_loop(W::AbstractArray, b::AbstractArray, stride::Tuple{Int,Int}, σ::Function, A::AbstractArray)
+function convolve_loop(W::AbstractArray, b::AbstractArray, stride::Tuple{Int,Int}, A::AbstractArray)
 	kx, ky, kd1, kd2 = size(W)
 	inx, iny, = size(A)
 	m, n = size_after_conv((inx, iny), stride, (kx, ky))
@@ -38,10 +38,11 @@ function convolve_loop(W::AbstractArray, b::AbstractArray, stride::Tuple{Int,Int
 		for (i´,x´) in enumerate(x:x+kx-1), (j´,y´) in enumerate(y:y+ky-1), l in 1:kd1
 			acc += W[i´,j´,l,k] * A[x´,y´,l]
 		end
-		output[i,j,k] = σ(b[k] + acc)
+		output[i,j,k] = b[k] + acc
 	end
 	return output
 end
+
 
 Random.seed!(1)
 conv_flux = Flux.Conv((5,5), 5=>10)
@@ -50,14 +51,14 @@ X = randn(100, 50, 5, 1)
 X_ = reshape(X, 100, 50, 5)
 
 sum(abs.(conv_flux(X) .- my_conv(X_).s))
-sum(abs.(convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, my_conv.σ, X_) .-
-	convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, my_conv.σ, X_)))
+sum(abs.(convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, X_) .-
+	convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, X_)))
 
 @btime conv_flux(X) # 5.526 ms (45 allocations: 692.05 KiB)
 @btime my_conv(X_) # 214.307 ms (1364570 allocations: 144.67 MiB)
 @btime convolve_slice(my_conv.W.s, my_conv.b.s, my_conv.stride, my_conv.σ, X_) # 17.400 ms (70659 allocations: 50.67 MiB)
-@btime convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, my_conv.σ, X_) # 12.331 ms (3 allocations: 345.11 KiB)
-@btime convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, my_conv.σ, X_) # 5.515 ms (3 allocations: 345.11 KiB) (inbounds macro)
+@btime convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, X_) # 12.331 ms (3 allocations: 345.11 KiB)
+@btime convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, X_) # 5.515 ms (3 allocations: 345.11 KiB) (inbounds macro)
 214.307 / 5.526 # faster than mine
 
 Y = Float32.(X)
@@ -79,3 +80,77 @@ Y = Float32.(X)
 (214.307 * 128) / 39.134 # faster than mine iter
 
 ################################################################################
+
+# without sigma
+function ∇convolve_loop(W::AbstractArray, b::AbstractArray, stride::Tuple{Int,Int}, A::AbstractArray, ∇::AbstractArray)
+	∇W = zeros(size(W)); ∇b=zeros(size(b)); ∇A = zeros(size(A))
+	kx, ky, kd1, kd2 = size(W)
+	inx, iny, = size(A)
+	m, n = size_after_conv((inx, iny), stride, (kx, ky))
+	@assert (m, n, kd2) == size(∇)
+	for i in 1:m, j in 1:n, k in 1:kd2
+		x = 1+(i-1)*stride[1]
+		y = 1+(j-1)*stride[2]
+		for (i´,x´) in enumerate(x:x+kx-1), (j´,y´) in enumerate(y:y+ky-1), l in 1:kd1
+			∇W[i´,j´,l,k] +=  A[x´,y´,l] * ∇[i,j,k]
+			∇A[x´,y´,l] += W[i´,j´,l, k] * ∇[i,j,k]
+		end
+		∇b[k] += ∇[i,j,k]
+	end
+	return ∇W, ∇b, ∇A
+end
+
+Random.seed!(1)
+conv_flux = Flux.Conv((5,5), 5=>10)
+X = randn(100, 50, 5, 1)
+X_ = reshape(X, 100, 50, 5)
+Y = Float32.(X)
+
+ps = Flux.params(conv_flux, X)
+gs = Flux.gradient(ps) do
+	sum(conv_flux(X))
+end
+gs[ps[1]]
+
+TX_ = DTensor(X_)
+my_conv = Conv(conv_flux)
+backward(sum(my_conv(TX_)))
+
+sum(abs.(conv_flux(X) .- my_conv(TX_).s))
+sum(abs.(gs[ps[3]] .- TX_.∇))
+sum(abs.(gs[ps[2]] .- my_conv.b.∇))
+sum(abs.(gs[ps[1]] .- flip(my_conv.W.∇)))
+
+
+∇ = ones(size(my_conv(X))[1:3])
+
+∇W, ∇b, ∇A = ∇convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, X_, ∇)
+
+
+sum(abs.(gs[ps[3]] .- ∇A))
+sum(abs.(gs[ps[2]] .- ∇b))
+sum(abs.(gs[ps[1]] .- flip(∇W)))
+
+sum(abs.(my_conv.W.∇ .- ∇W))
+
+
+
+@btime begin
+	TX_.∇ .= 0
+	my_conv.W.∇ .= 0
+	my_conv.b.∇ .= 0
+	backward(sum(my_conv(TX_)))
+end # 452.639 ms (1907797 allocations: 316.44 MiB)
+
+@btime begin
+	TX_.∇ .= 0
+	my_conv.W.∇ .= 0
+	my_conv.b.∇ .= 0
+	A = DTensor(convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, TX_.s))
+	backward(sum(A))
+	∇W, ∇b, ∇TX_ = ∇convolve_loop(my_conv.W.s, my_conv.b.s, my_conv.stride, X_, A.∇)
+
+	TX_.∇ .+= ∇TX_
+	my_conv.W.∇ .+= ∇W
+	my_conv.b.∇ .+= ∇b
+end # 51.536 ms (39 allocations: 897.20 KiB)
