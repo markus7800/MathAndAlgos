ba0320640721542f0cf94dc38d49199e681f4b99
Markus Boeck <markus.boeck7800@hotmail.com>
2020-11-22 21:09:57 +0100

    cifar gpu works /wo accuracy

--- Algorithms/ADNN/cifar10/cifar10.jl
+++ Algorithms/ADNN/cifar10/cifar10.jl
@@ -12,14 +12,20 @@ using StatsBase
 using Dates
 using Printf
 
+using CUDA
+CUDA.has_cuda()
+CUDA.version()
+CUDA.device()
+
+
 # Function to convert the RGB image to Float32 Arrays
 function getarray(X)
     Float32.(permutedims(channelview(X), (2, 3, 1)))
 end
 
 # statistics
-const μ  = reshape(Float32[0.4914, 0.4822, 0.4465],1,1,3)
-const σ = reshape(Float32[0.2023, 0.1994, 0.2010],1,1,3)
+const CIFAR10_MEANS  = reshape(Float32[0.4914, 0.4822, 0.4465],1,1,3)
+const CIFAR10_SDS = reshape(Float32[0.2023, 0.1994, 0.2010],1,1,3)
 
 function get_processed_data(;batchsize=128, splitr_ = 0.1, N=50_000)
     # Fetching the train and validation data and getting them into proper shape
@@ -30,23 +36,23 @@ function get_processed_data(;batchsize=128, splitr_ = 0.1, N=50_000)
     labels = onehotbatch([X[i].ground_truth.class for i in 1:N],1:10)
 
     train_pop = Int((1-splitr_)* N)
-    train = gpu.([(cat(imgs[i]..., dims = 4), labels[:,i]) for i in partition(1:train_pop, batchsize)])
+    train = [(cat(imgs[i]..., dims = 4), labels[:,i]) for i in partition(1:train_pop, batchsize)]
     valset = collect(train_pop+1:N)
-    valX = cat(imgs[valset]..., dims = 4) |> gpu
-    valY = labels[:, valset] |> gpu
+    valX = cat(imgs[valset]..., dims = 4)
+    valY = labels[:, valset]
 
     val = (valX,valY)
     return train, val
 end
 
-function get_test_data(N=1000)
+function get_test_data(N=10_000)
     # Fetch the test data from Metalhead and get it into proper shape.
     test = valimgs(CIFAR10)
 
     # CIFAR-10 does not specify a validation set so valimgs fetch the testdata instead of testimgs
     testimgs = [getarray(test[i].img) for i in 1:N] # 10_000 available
-    testY = onehotbatch([test[i].ground_truth.class for i in 1:1000], 1:10) |> gpu
-    testX = cat(testimgs..., dims = 4) |> gpu
+    testY = onehotbatch([test[i].ground_truth.class for i in 1:1000], 1:10)
+    testX = cat(testimgs..., dims = 4)
 
     test = (testX,testY)
     return test
@@ -60,12 +66,13 @@ struct ConvBlock
     mp::MaxPool
     pool::Bool
     σ
-    function ConvBlock(ch::Pair{Int,Int}, size=(3,3), pad=(1,1); pool=false)
-        conv = Conv(size, ch, pad=pad)
-        bn = BatchNorm(ch[2])
-        mp = MaxPool((2,2))
-        return new(conv,bn,mp,pool,relu)
-    end
+end
+
+function ConvBlock(ch::Pair{Int,Int}, size=(3,3), pad=(1,1); pool=false)
+    conv = Conv(size, ch, pad=pad)
+    bn = BatchNorm(ch[2])
+    mp = MaxPool((2,2))
+    return ConvBlock(conv,bn,mp,pool,relu)
 end
 
 function (CB::ConvBlock)(x)
@@ -83,11 +90,12 @@ end
 struct ResBlock
     conv_block_1::ConvBlock
     conv_block_2::ConvBlock
-    function ResBlock(ch::Pair{Int,Pair{Int,Int}})
-        inc, ch2 = ch
-        midc, out = ch2
-        new(ConvBlock(inc=>midc), ConvBlock(midc=>out))
-    end
+end
+
+function ResBlock(ch::Pair{Int,Pair{Int,Int}})
+    inc, ch2 = ch
+    midc, out = ch2
+    ResBlock(ConvBlock(inc=>midc), ConvBlock(midc=>out))
 end
 
 function (RB::ResBlock)(x)
@@ -95,6 +103,21 @@ function (RB::ResBlock)(x)
     y = RB.conv_block_2(y)
     return y + x
 end
+
+@functor ResBlock
+
+function ResNet9(in_channels, n_class)
+    prep = ConvBlock(in_channels => 64)
+    conv1 = ConvBlock(64=>128, pool=true)
+    res1 = ResBlock(128=>128=>128)
+    conv2 = ConvBlock(128=>256, pool=true)
+    conv3 = ConvBlock(256=>512, pool=true)
+    res2 = ResBlock(512=>512=>512)
+    classifier = MaxPool((4,4)), flatten, Dense(512, n_class)
+
+    return Chain(prep, conv1, res1, conv2, conv3, res2, classifier...)
+end
+
 
 import Base.show
 function Base.show(io::IO, l::Conv)
@@ -130,21 +153,6 @@ function show(io::IO, RB::ResBlock)
     println(io, "-Residual Block")
     println(io, RB.conv_block_1)
     print(io, RB.conv_block_1)
-end
-
-@functor ResBlock
-
-
-function ResNet9(in_channels, n_class)
-    prep = ConvBlock(in_channels => 64)
-    conv1 = ConvBlock(64=>128, pool=true)
-    res1 = ResBlock(128=>128=>128)
-    conv2 = ConvBlock(128=>256, pool=true)
-    conv3 = ConvBlock(256=>512, pool=true)
-    res2 = ResBlock(512=>512=>512)
-    classifier = MaxPool((4,4)), flatten, Dense(512, n_class)
-
-    return Chain(prep, conv1, res1, conv2, conv3, res2, classifier...)
 end
 
 function pretty_print(c::Chain)
@@ -281,23 +289,35 @@ function accuracy(x, y, m; batchsize=100)
     s = 0
     # batch to decrease ram pressure on macbook
     for is in partition(1:N, batchsize)
-        s += sum(onecold(cpu(m(x[:,:,:,is])), 1:10) .== onecold(cpu(y[is]), 1:10))
+        s += sum(onecold(cpu(m(gpu(x[:,:,:,is]))), 1:10) .== onecold(cpu(y[is]), 1:10))
     end
     return s/N
 end
 
-function train(; epochs=8, normalize=false, batchsize=400, permute=true, schedule_lr=true)
+function train(; epochs=8, normalize=false, batchsize=400, permute=true, schedule_lr=true, enable_gpu=false)
 
     @info("Load training data.")
     train_set, val_set = get_processed_data(batchsize=batchsize)
+
+
     if normalize
         @info("Normalizing training data.")
-        map!(x -> ((x[1] .- μ) ./ σ, x[2]), train_set, train_set)
-        val_set = (val_set[1] .- μ) ./ σ, val_set[2]
+        map!(x -> ((x[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, x[2]), train_set, train_set)
+        val_set = (val_set[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, val_set[2]
     end
+
+    # if enable_gpu
+    #     # leave train set on cpu for preprocessing
+    #     val_set = gpu(val_set)
+    # end
+
     @info("Constructing Model.")
+
     Random.seed!(1)
     m = ResNet9(3,10)
+    if enable_gpu
+        m = gpu(m)
+    end
 
     sqnorm(x) = sum(abs2, x)
     L2_penalty(m) = sum(sqnorm, params(m))
@@ -324,16 +344,27 @@ function train(; epochs=8, normalize=false, batchsize=400, permute=true, schedul
         @info "Epoch $(epoch):"
 
         if permute
+            # do preprocessing on CPU
             epoch_set, count, t = random_permute_set(train_set)
             @info @sprintf "\tRandomly permuted %d images in %.2f seconds." count t
         else
             epoch_set = train_set
         end
 
+        if enable_gpu
+            epoch_set = gpu.(epoch_set)
+        end
+
         v,t_train, = @timed Flux.train!(loss, params(m), epoch_set, opt, cb=cb)
         @info "\tEpoch finished in $(Int(round(t_train/60))) minutes."
 
-        v,t_val, = @timed (@info "\tAccuracy on validation: $(accuracy(val_set..., m))")
+        v,t_val, = @timed if enable_gpu
+            acc = accuracy(val_set..., m, batchsize=1000)
+        else
+            acc = accuracy(val_set..., m)
+        end
+
+        @info "\tAccuracy on validation: $acc"
         @info "\tValidated in $(Int(round(t_val))) seconds."
 
         if schedule_lr
@@ -356,13 +387,13 @@ function test(m; normalize=false)
     test_data = get_test_data()
 
     if normalize
-        test_data = (test_data[1] .- μ) ./ σ, test_data[2]
+        test_data = (test_data[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, test_data[2]
     end
     # Print the final accuracy
     @show(accuracy(test_data..., m))
 end
 
-m = train(normalize=true, batchsize=128, epochs=5)
+m = train(normalize=true, batchsize=400, epochs=15, enable_gpu=true)
 @time acc = test(m,normalize=true)
 
 ps = params(m)
@@ -374,7 +405,7 @@ BSON.@save joinpath("cifar_conv2.bson") params=ps
 # 40 min pro epoch
 # 18s for acc on test -> 1000 imgs
 
-#=
+
 using Plots
 
 test_data = get_test_data()
@@ -424,5 +455,3 @@ ps = params(m)
 sum(map(p->prod(size(p)), ps))
 
 @time accuracy(val_set..., m)
-
-=#
