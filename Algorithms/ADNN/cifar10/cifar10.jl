85f28cbfbb5300ed84cb84adf6daa13d4f963e0d
Markus Boeck <markus.boeck7800@hotmail.com>
2020-11-24 01:24:00 +0100

    should work now

--- Algorithms/ADNN/cifar10/cifar10.jl
+++ Algorithms/ADNN/cifar10/cifar10.jl
@@ -47,14 +47,14 @@ function get_processed_data(;batchsize=128, splitr_ = 0.1, N=50_000)
     return train, val
 end
 
-function get_test_data(;N=1_000, batchsize=400)
+function get_test_data(;N=10_000, batchsize=400)
     # Fetch the test data from Metalhead and get it into proper shape.
     test = valimgs(CIFAR10)
 
     # CIFAR-10 does not specify a validation set so valimgs fetch the testdata instead of testimgs
     testimgs = [getarray(test[i].img) for i in 1:N] # 10_000 available
     labels = onehotbatch([test[i].ground_truth.class for i in 1:N], 1:10)
-    test = [(cat(testimgs[i]..., dims = 4), testY[:,i]) for i in partition(1:N, batchsize)]
+    test = [(cat(testimgs[i]..., dims = 4), labels[:,i]) for i in partition(1:N, batchsize)]
 
     return test
 end
@@ -121,42 +121,29 @@ end
 
 include("show.jl")
 
-
-# x is one big batch
-# function accuracy(x, y, m; batchsize=1000, enable_gpu=false)
-#     N = size(x,4)
-#     s = 0
-#     _pu = enable_gpu ? gpu : cpu
-#     # batch to decrease ram demand
-#     for is in partition(1:N, batchsize)
-#         s += sum(onecold(cpu(m(_pu(x[:,:,:,is]))), 1:10) .== onecold(cpu(y[:,is]), 1:10))
-#     end
-#     return s/N
-# end
-
 # X is batched (list of tuples), ram friendly
-function accuracy3(X, m)
+function accuracy(X, m)
     N = sum(map(b -> size(b[1],4), X))
     println("N: ", N)
     s = 0
-    for batch in X
+    @progress for batch in X
         s += sum(onecold(cpu(m(batch[1])), 1:10) .== onecold(cpu(batch[2]), 1:10))
     end
     return s/N
 end
 
-function train(; epochs=8, normalize=false, batchsize=400, permute=true, schedule_lr=true, enable_gpu=false)
+function train(; epochs=8, normalize=false, batchsize=400, λ = 1f-4,
+                permute=true, schedule_lr=true, enable_gpu=false)
+
     Random.seed!(1)
 
     @info("Load training data.")
     train_set, val_set = get_processed_data(batchsize=batchsize)
-    test_set = get_test_data()
 
     if normalize
         @info("Normalizing training data.")
         map!(x -> ((x[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, x[2]), train_set, train_set)
-        val_set = (val_set[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, val_set[2]
-        test_set = (test_set[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, test_set[2]
+        map!(x -> ((x[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, x[2]), val_set, val_set)
     end
 
 
@@ -166,41 +153,35 @@ function train(; epochs=8, normalize=false, batchsize=400, permute=true, schedul
     m = ResNet9(3,10)
     if enable_gpu
         m = gpu(m)
-        train_set = gpu.(train_set)
-        val_set = gpu(val_set)
-        test_set = gpu(test_set)
+        # leave train set on cpu for permuting
+        val_set = gpu.(val_set)
     end
 
     sqnorm(x) = sum(abs2, x)
     L2_penalty(m) = sum(sqnorm, params(m))
-    λ = 1f-4
     loss(x, y) = logitcrossentropy(m(x), y) + λ * L2_penalty(m)
 
-    # batches = length(train_set)
-    # @info("Number of epochs: $epochs.")
-    # @info("Number of batches: $batches.")
-    # ocs = OneCycleSchedular(max_lr=0.01, epochs=epochs, batches=batches)
-    # if schedule_lr
-    #     opt = ADAM(get_lr(ocs))
-    #     function cb()
-    #         step!(ocs)
-    #         opt.eta = get_lr(ocs)
-    #     end
-    # else
-    #     opt = ADAM(0.001)
-    #     cb = () -> ()
-    # end
+    batches = length(train_set)
+    @info("Number of epochs: $epochs.")
+    @info("Number of batches: $batches.")
+    ocs = OneCycleSchedular(max_lr=0.01, epochs=epochs, batches=batches)
+    if schedule_lr
+        opt = ADAM(get_lr(ocs))
+        function cb()
+            step!(ocs)
+            opt.eta = get_lr(ocs)
+        end
+    else
+        opt = ADAM(0.001)
+        cb = () -> ()
+    end
 
-    opt = ADAM(0.001)
+    best_acc_val = -Inf
+    acc_vals = []
+    acc_trains = []
 
     @info "Started training at: " * Dates.format(now(), "yyyy/m/d HH:MM")
-    for epoch in 1:epochs
-        @info "Epoch $(epoch):"
-        # v,t, = @timed Flux.train!(loss, params(m), train_set, opt)
-        # @info "Finished in $(Int(round(t/60))) minutes."
-        # v,t, = @timed (@info "Accuracy on validation: $(accuracy(test_set..., m))")
-        # @info "Validated in $t seconds."
-
+    Flux.@epochs epochs begin
         if permute
             # do preprocessing on CPU
             epoch_set, count, t = random_permute_set(train_set)
@@ -213,141 +194,50 @@ function train(; epochs=8, normalize=false, batchsize=400, permute=true, schedul
             epoch_set = gpu.(epoch_set)
         end
 
-        v,t_train, = @timed Flux.train!(loss, params(m), epoch_set, opt)
-        @info "\tEpoch finished in $(Int(round(t_train/60))) minutes."
+        v, t_epoch, = @timed Flux.train!(loss, params(m), epoch_set, opt, cb=cb)
+        @info "\tEpoch finished in $(Int(round(t_epoch/60))) minutes."
 
-        v,t_val, = @timed if enable_gpu
-            acc = accuracy(val_set..., m, batchsize=1000, enable_gpu = true)
-        else
-            acc = accuracy(val_set..., m)
+        acc_train, t_train, = @timed accuracy(epoch_set, m)
+        @info @sprintf "\tAccuracy on train: %.4f (%.2f second)s." acc_train t_train
+        push!(acc_trains, acc_train)
+
+        acc_val, t_val, = @timed accuracy(val_set, m)
+        @info @sprintf "\tAccuracy on validation: %.4f (%.2f second)s." acc_val t_val
+        push!(acc_vals, acc_val)
+        if acc_val > best_acc_val
+            best_acc_val = acc_val
+            BSON.@save "Algorithms/ADNN/cifar10/temp.bson" model=cpu(m)
+            @info "\tBest accuracy reaced. Model saved."
         end
-
-        @info "\tAccuracy on validation: $acc"
-        @info "\tValidated in $(Int(round(t_val))) seconds."
 
         if schedule_lr
             @info @sprintf "\tLearning rate is at %.4f" opt.eta
         end
-
-        if epoch < epochs
-            t = t_train + t_val
-            ETA = now() + Second(Int(round(t*(epochs-epoch))))
-            @info "\tExpected finishing time: " * Dates.format(ETA, "yyyy/m/d HH:MM")
-        end
     end
     @info "Finished training at: " * Dates.format(now(), "yyyy/m/d HH:MM")
 
-    return m
+    return m, acc_trains, acc_vals
 end
 
 
-function test(m; normalize=false, enable_gpu=false, N=1_000)
-    test_data = get_test_data(N)
+function test(m; normalize=false, enable_gpu=false, N=10_000, batchsize=128)
+    test_data = get_test_data(N=N, batchsize=batchsize)
     @info("Test data loaded.")
 
     if normalize
-        test_data = (test_data[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, test_data[2]
+        map!(x -> ((x[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, x[2]), test_data, test_data)
+    end
+
+    if enable_gpu
+        test_data = gpu(test_data)
     end
 
     # Print the final accuracy
-    acc,t = @timed accuracy(test_data..., m, enable_gpu=enable_gpu)
+    acc,t = @timed accuracy3(test_data, m)
     @info("Accuracy $acc evaluated in $t seconds.")
 end
 
-m = train(normalize=true, batchsize=400, epochs=5,
+m, acc_trains, acc_vals = train(normalize=true, batchsize=400, epochs=5,
     enable_gpu=true, permute=false, schedule_lr=false)
 
-@time acc = test(cpu(m),normalize=true, enable_gpu=false) # 14s
-@time acc = test(m,normalize=true, enable_gpu=true) # 1.3
-
-train_set_, val_set_ = get_processed_data(;batchsize=128)
-map!(x -> ((x[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, x[2]), train_set_, train_set_)
-val_set_ = ((val_set_[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, val_set_[2])
-accuracy3(gpu.(train_set_), m)
-accuracy(val_set_..., m, enable_gpu=true)
-
-
-train_set_2, val_set_2 = get_processed_data(;batchsize=128)
-map!(x -> ((x[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, x[2]), train_set_2, train_set_2)
-map!(x -> ((x[1] .- CIFAR10_MEANS) ./ CIFAR10_SDS, x[2]), val_set_2, val_set_2)
-accuracy3(gpu.(train_set_2), m)
-accuracy3(gpu.(val_set_2), m)
-
-
-using CUDA
-
-d = gpu(Conv((10,10), 2=>3))
-d = fmap(CUDA.cu, Conv((10,10), 2=>3))
-
-d = gpu(ConvBlock(4=>4))
-typeof(d.conv.weight
-
-d = gpu(ResBlock(4=>4=>4))
-typeof(d.conv_block_1.conv.weight)
-
-ps = params(m)
-n_params = sum(map(p->prod(size(p)), ps))
-BSON.@save joinpath("cifar_conv2.bson") params=ps
-@info "Saved model."
-
-# MACBOOK
-# 40 min pro epoch
-# 18s for acc on test -> 1000 imgs
-
-
-
-
-
-
-
-
-
-using Plots
-
-test_data = get_test_data()
-img = test_data[1][:,:,:,1]
-
-function img_from_float(X)
-    n, m, = size(X)
-    img = Array{RGB}(undef, n, m)
-    for i in 1:n, j in 1:m
-        img[i,j] = RGB((X[i,j,:])...)
-    end
-    img
-end
-
-
-pussy = train_set[1][1][:,:,:,10]
-using BenchmarkTools
-@btime random_permute(pussy)
-r_pussy = random_permute(pussy)
-img_from_float(pussy)
-img_from_float(r_pussy)
-
-k = 0
-permuted_set = random_permute_set(train_set)
-
-μ, σ = reshape([0.4914, 0.4822, 0.4465],1,1,3), reshape([0.2023, 0.1994, 0.2010],1,1,3)
-img_from_float((img .- μ) ./ σ)
-
-lr0 = 0.001
-lr_max = 0.01
-lr_end =
-plot(t->(lr_max-lr0)*t / cos(π*t) + lr0, 0, 1)
-
-
-train_set, val_set = get_processed_data(batchsize=128)
-
-for i in 1:10
-    display(img_from_float(permuted_set[1][1][:,:,:,i]))
-end
-
-trainimgs(CIFAR10)
-
-Random.seed!(1)
-m = ResNet9(3,10)
-
-ps = params(m)
-sum(map(p->prod(size(p)), ps))
-
-@time accuracy(val_set..., m)
+@time acc = test(m, normalize=true, enable_gpu=true)
