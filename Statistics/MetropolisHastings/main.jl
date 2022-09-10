27493f142f31a12fad11f1c2c0c3330de17d0bd4
Markus Boeck <markus.boeck7800@hotmail.com>
2022-09-10 19:55:51 -0400

    mh

--- Statistics/MetropolisHastings/main.jl
+++ Statistics/MetropolisHastings/main.jl
@@ -1,34 +1,5 @@
-using ProgressMeter
 
-function Metropolis_nD(pdf::Function; # needs not be normalised
-               n_iter::Int, var::Float64, q_init::Vector{Float64})::Tuple
-
-    local d = length(q_init)
-
-    local q = Array{Float64}(undef, n_iter, d)
-    q[1,:] = q_init
-    accepted = 0
-
-    ProgressMeter.@showprogress  1 "Iterations: " for n in 1:n_iter-1
-
-        # gaussian proposal qq ∼ N(q[n], var)
-        local qq::Vector{Float64} = q[n,:] .+ sqrt(var) * randn(Float64, d)
-
-        # proposal function is symmetric so it cancels in the fraction
-        local A::Float64 = min(1, pdf(qq) / pdf(q[n,:]))
-
-        if rand(Float64) ≤ A
-            q[n+1,:] = qq
-            accepted += 1
-        else
-            q[n+1,:] = q[n,:]
-        end
-    end
-
-    acceptance_ratio = accepted / (n_iter-1)
-
-    return q, acceptance_ratio
-end
+include("MetropolisND.jl")
 
 using Plots
 using DataFrames
